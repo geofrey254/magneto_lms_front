1e0a17f69179ab41111fe2e965c3eebbe6c86e56
geofrey <devongeoffreymaina@gmail.com>
2024-12-29 12:49:16 +0300

    Social sign in Adding to Db

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -29,6 +29,38 @@ const authHandler = NextAuth({
 
       return token;
     },
+    async signIn({ user, account }) {
+      const isAllowedToSignIn = true;
+      if (isAllowedToSignIn) {
+        if (account?.provider) {
+          try {
+            const res = await fetch(
+              `${process.env.NEXT_PUBLIC_AUTH}/magneto/save_user/`,
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ user }),
+              }
+            );
+
+            if (!res.ok) {
+              const errorText = await res.text(); // Handle non-JSON responses
+              console.error("Failed to save user:", errorText);
+              return false;
+            }
+          } catch (error) {
+            console.error("Error during user save:", error);
+            return false;
+          }
+        }
+        return true;
+      } else {
+        console.warn("Sign-in not allowed for this user");
+        return "/signin";
+      }
+    },
   },
 });
 
